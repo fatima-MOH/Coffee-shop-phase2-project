d4fce937c8dd3bc98b133edd408883a72324ec43
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-05-21 18:16:06 -0400

    done

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <section id="home">
+            <h1 style={{ color: "firebrick" }}>
+             welcome to The Friendly Bean
+            </h1>
+        </section>
+    );
+}
+
+export default Home;

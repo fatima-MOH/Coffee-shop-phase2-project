da461ca6b3bc3b348ef3fc946ceb20e12af58aeb
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-05-22 14:48:40 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 
-import React,{ useState} from "react";
+import React,{useEffect, useState} from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import ItemForm from "./ItemForm";
@@ -7,15 +7,50 @@ import MenuList from "./MenuList";
 
 function App() {
     const [page, setPage] = useState("/")
+
+    const [items, setItems] = useState([]);
+    console.log(items);
+
+    
+// Add useEffect hook
+    useEffect(() => {
+        fetch("http://localhost:3001/coffies")
+          .then((r) => r.json())
+          .then((items) => setItems(items));
+      }, []);
+
+
+   
+//handleDeleteItem
+
+    function handleDeleteItem(id) {
+        fetch(` http://localhost:3001/coffies/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        const updatedQuestions = items.filter((item) => item.id !== id);
+        setItems(updatedQuestions);
+      })
+
+        
+    }
+    //handleAddItem
+
+    function handleAddItem(newItem) {
+        setItems([...items, newItem]);
+    }
+
+
     
     function getCurrentPage() {
         switch(page) {
             case "/":
                 return <Home />
             case "/MenuList":
-                return <MenuList />
+                return <MenuList  handleDeleteItem={handleDeleteItem} items={items}/>
             case "/ItemForm":
-                return <ItemForm />
+                return <ItemForm handleAddItem={handleAddItem}/>
             default:
                 return <h1>404 not found</h1>
         }
--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,61 +1,28 @@
-import React, {useEffect, useState } from 'react';
+import React from 'react';
 import { Grid } from '@material-ui/core';
 import Cards from "./Cards"
-import ItemForm from "./ItemForm"
 
 
-function MenuList() {
-
-    const [items, setItems] = useState([]);
-    console.log(items);
-
-// Add useEffect hook
-    useEffect(() => {
-        fetch("http://localhost:3001/coffies")
-          .then((r) => r.json())
-          .then((items) => setItems(items));
-      }, []);
+function MenuList({ handleDeleteItem ,items }) {
 
 
-    function handleAddItem(newItem) {
-        setItems([...items, newItem]);
-    }
+  const CardsItems = items.map((q) => <Grid key={q.id} item xs={3}>
+    <Cards item={q} handleDeleteItem={handleDeleteItem} /></Grid>)
+  return (
+    <div>
+
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        spacing={3}
+      >{CardsItems}</Grid>
 
 
-    function handleDeleteItem(id) {
-        fetch(` http://localhost:3001/coffies/${id}`, {
-      method: "DELETE",
-    })
-      .then((r) => r.json())
-      .then(() => {
-        const updatedQuestions = items.filter((item) => item.id !== id);
-        setItems(updatedQuestions);
-      })
-
-        
-    }
+    </div>
 
 
-
-
-
-    const CardsItems = items.map((q) => <Grid key={q.id} item xs={3}>
-        <Cards item={q} handleDeleteItem={handleDeleteItem} /></Grid>)
-    return (
-        <div>
-            <ItemForm handleAddItem={handleAddItem} />
-            <Grid
-            container
-            direction="row"
-            justifyContent="flex-start"
-            alignItems="flex-start"
-            spacing={3}
-            >{CardsItems}</Grid>
-            
-
-        </div>
-
-
-    );
+  );
 }
 export default MenuList
